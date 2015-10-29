77a81a6d4fc0fec784d269824a21ae6d854049d8
C.W. Betts <computers57@hotmail.com>
2015-10-29 16:55:37 -0600

    Begin porting the Preferences dialog to Cocoa.

--- neo/sys/osx/PreferencesDialog.cpp
+++ neo/sys/osx/PreferencesDialog.cpp
@@ -27,11 +27,11 @@ If you have questions concerning this license or the applicable additional terms
 */
 
 #include "../../idlib/precompiled.h"
+#include <list>
+#include <set>
 #include <Carbon/Carbon.h>
 #include "PreferencesDialog.h"
 #include "PickMonitor.h"
-#include <list>
-#include <set>
 
 static idCVar r_stretched( "r_stretched", "0", CVAR_ARCHIVE | CVAR_BOOL, "Used stretched resolution" );
 

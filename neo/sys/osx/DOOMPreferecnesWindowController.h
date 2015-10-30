8d9325e8468a710f7ff78d942c5601143dc9bb33
C.W. Betts <computers57@hotmail.com>
2015-10-30 02:41:44 -0600

    The last of the unneeded metadata should be removed now.

---
+++ neo/sys/osx/DOOMPreferecnesWindowController.h
@@ -0,0 +1,13 @@
+//
+//  DOOMPreferecnesWindowController.h
+//  Doom3
+//
+//  Created by C.W. Betts on 10/30/15.
+//  Copyright © 2015 id Software LLC. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface DOOMPreferecnesWindowController : NSWindowController
+
+@end
---
+++ neo/sys/osx/DOOMPreferecnesWindowController.mm
@@ -0,0 +1,13 @@
+//
+//  DOOMPreferecnesWindowController.m
+//  Doom3
+//
+//  Created by C.W. Betts on 10/30/15.
+//  Copyright © 2015 id Software LLC. All rights reserved.
+//
+
+#import "DOOMPreferecnesWindowController.h"
+
+@implementation DOOMPreferecnesWindowController
+
+@end
